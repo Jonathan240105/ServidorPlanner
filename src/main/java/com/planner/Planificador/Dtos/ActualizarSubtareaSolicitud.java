6af32a685aa803ed801e6556b0b704b4b33bdc51
Jonathan240105 <jonathan.quito@live.u-tad.com>
2026-04-22 11:59:59 +0200

    Commit añadida entidad subtarea y sus endpoints

---
+++ src/main/java/com/planner/Planificador/Dtos/ActualizarSubtareaSolicitud.java
@@ -0,0 +1,35 @@
+package com.planner.Planificador.Dtos;
+
+public class ActualizarSubtareaSolicitud {
+
+	private String titulo;
+	private Boolean estado;
+
+	// Constructores
+	public ActualizarSubtareaSolicitud() {
+		super();
+	}
+
+	public ActualizarSubtareaSolicitud(String titulo, Boolean estado) {
+		super();
+		this.titulo = titulo;
+		this.estado = estado;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+
+}
